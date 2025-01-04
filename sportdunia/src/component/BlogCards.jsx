6290285298d95f0c8415c0ae765ebffb028f0deb
Vivek kumar <viveka9415@gmail.com>
2025-01-04 08:23:46 +0530

    fromat cards section-> author ,title , description, images , publish date

--- sportdunia/src/component/BlogCards.jsx
+++ sportdunia/src/component/BlogCards.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function BlogCards({ blogs }) {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("en-IN", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+      timeZone: "Asia/Kolkata",
+    }).format(date);
+  };
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {blogs.map((blog, index) => (
@@ -18,6 +31,10 @@ export default function BlogCards({ blogs }) {
             />
           </div>
           <h3 className="mt-2 text-lg font-semibold">{blog.title}</h3>
+          <p className="mt-1 text-gray-600 text-sm">{`Author: ${blog.author}`}</p>
+          <p className="mt-1 text-gray-600 text-sm">
+            {formatDate(blog.publishedAt)}
+          </p>
           <p className="mt-1 text-gray-600 text-sm">{blog.description}</p>
         </Link>
       ))}
